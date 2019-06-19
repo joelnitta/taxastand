c38412cb41437aca59c349a0fd678866f3d6d357
Joel Nitta <joelnitta@gmail.com>
2019-06-19 11:12:23 +0900

    Add filmy_taxonomy data

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Taxonomy of filmy ferns (family Hymenophyllaceae)
+#'
+#' A dataset containing taxonomic names and associated metadata for the
+#' fern family Hymenophyllaceae. Downloaded from the
+#' [Catalog of Life](http://www.catalogueoflife.org/), Version 1.5.
+#' All columns formatted according to
+#' [Darwin Core standard](https://dwc.tdwg.org/terms/). Only includes taxa
+#' at the species or infraspecies level.
+#'
+#' @format A data frame with 2729 rows and 31 variables.
+#'
+#' @source <http://www.catalogueoflife.org/>
+"filmy_taxonomy"
---
+++ data-raw/filmy_taxonomy.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+
+# Load the example standard taxonomy for resolving names.
+
+# The example standard taxonomy is the family Hymenophyllaceae from
+# Catalog of Life (CoL). CoL provides persistant links to database dumps.
+# This one was obtained by selecting "Hymenophyllaceae" for "family"
+# and "Complete data" on http://www.catalogueoflife.org/DCA_Export/index.php
+# on 2019-06-19
+
+# Download the zip file
+temp_dir <- fs::dir_create(tempdir())
+download.file(
+  "http://www.catalogueoflife.org/DCA_Export/zip/archive-family-hymenophyllaceae-bl3.zip",
+  fs::path(temp_dir, "archive-genus-vandenboschia-bl3.zip")
+)
+
+# Unzip
+unzip(
+  fs::path(temp_dir, "archive-genus-vandenboschia-bl3.zip"),
+  exdir = temp_dir)
+
+# Read in taxonomy table, keep only
+# names at species rank and below
+# (warnings are produced because names at genus level
+# and above have NA for many fields).
+filmy_taxonomy <- read_tsv(fs::path(temp_dir, "taxa.txt")) %>%
+  filter(str_detect(taxonRank, "species"))
+
+usethis::use_data(filmy_taxonomy)
